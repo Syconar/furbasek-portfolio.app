ccefe687b84a5416c202fcbde34257fc3b79d6da
Syconar <urbasekfilip@gmail.com>
2025-07-05 19:04:32 +0200

    Navbar finnished

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Navbar will hide when scrolling down
+const navbar = document.getElementById("navbar");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+        navbar.classList.add("navbar-hidden");
+    } else {
+        navbar.classList.remove("navbar-hidden");
+    }
+    
+
+    lastScrollY = window.scrollY
+});
