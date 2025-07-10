424686e2f5f7200ddaa58393e4f92a0b6144336c
Syconar <urbasekfilip@gmail.com>
2025-07-10 17:06:18 +0200

    language selection added

--- js/themes.js
+++ js/themes.js
@@ -1,3 +1,4 @@
+// SWITCHING BETWEEN LIGHTMODE AND DARKMODE
 let lightmode = localStorage.getItem("lightmode");
 const themeSwitch = document.getElementById("nav-btn");
 
@@ -15,15 +16,59 @@ const disableLightmode = () => {
 
 // If the lightmode is active, save the theme to local storage
 if(lightmode === "active") {
-    enableLightmode()
+    enableLightmode();
 }
 
 // When click on the btn, change the theme
 themeSwitch.addEventListener("click", () => {
     lightmode = localStorage.getItem("lightmode") // save the selected theme
     if(lightmode !== "active"){
-        enableLightmode()
+        enableLightmode();
     } else {
-        disableLightmode()
+        disableLightmode();
     }
 });
+
+
+
+// SWITCHING BETWEEN ENGLISH AND CZECH LANGUAGES
+function setLanguage(lang) {
+    // Save language to localStorage
+    localStorage.setItem("lang", lang);
+
+    //  Load the correct JSON file with language
+    let langFile = lang === "cs" ? "js/cs-lang-data.json" : "js/en-lang-data.json";
+
+    fetch(langFile).then(response => response.json()).then(data => {
+        // For each element with data-i18l, sert its coresponding text
+        document.querySelectorAll("[data-i18l]").forEach(el => {
+            let key = el.getAttribute("data-i18l");
+            if (data[key]) {
+                el.textContent = data[key];
+            }
+        });
+    });
+}
+
+// On page load, set language from localStorage or default to "cs"
+document.addEventListener("DOMContentLoaded", () =>{
+    let lang = localStorage.getItem("lang") || "cs";
+    setLanguage(lang);
+
+    // Update "hidden" class on buttons
+    document.querySelectorAll(".nav-lang a").forEach(btn => {
+        btn.classList.toggle("hidden", btn.textContent.trim().toLowerCase() === (lang === "cs" ? "cze" : "eng"));
+    });
+});
+
+// Expose function for onclick
+window.changeLanguage = function(lang) {
+    setLanguage(lang);
+
+    // Update hidden class on buttons
+    document.querySelectorAll(".nav-lang a").forEach(btn => {
+        btn.classList.toggle("hidden", btn.textContent.trim().toLowerCase() === (lang === "cs" ? "cze" : "eng"));
+    });
+
+    return false;
+};
